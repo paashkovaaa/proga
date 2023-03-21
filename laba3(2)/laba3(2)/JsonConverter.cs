39e52c5a8adc3d080b5979d882627baac0b5c093
paashkovaaa <105235620+paashkovaaa@users.noreply.github.com>
2023-03-21 21:14:36 +0200

    Add files via upload

---
+++ laba3(2)/laba3(2)/JsonConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using laba3;
+static class JsonConverter
+{
+    public static string Serialize(Polynomial polynomial)
+    {
+        return JsonConvert.SerializeObject(polynomial);
+    }
+    public static Polynomial? Deserialize(string json)
+    {
+        return JsonConvert.DeserializeObject<Polynomial>(json);
+    }
+}
