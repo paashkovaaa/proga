9f9eead22695fe814b83d773a509d22e279a6592
paashkovaaa <105235620+paashkovaaa@users.noreply.github.com>
2023-04-30 14:52:31 +0300

    Add files via upload

---
+++ Book/Book/Controllers/BookController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly bookClient _client;
+
+        public BookController(bookClient client)
+        {
+            _client = client;
+        }
+
+        [HttpGet("{query}")]
+        public async Task<IActionResult> Search(string query)
+        {
+            var books = await _client.SearchBooksAsync(query);
+
+            if (books != null)
+            {
+                return Ok(books);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
+
---
+++ Book/Book/bookModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Book
+{
+    public class BookModel
+    {
+        public List<BookModel> Items { get; set; }
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public List<string> Authors { get; set; }
+        public string Description { get; set; }
+        public double Rating { get; set; }
+        public string Review { get; set; }
+    }
+}
